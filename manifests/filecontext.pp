cc101d750dd118f2cce303dd36092ecb2089042a
mtward <mtward@users.noreply.github.com>
2015-08-21 09:02:33 -0400

    update filecontext.pp
    make sure fcontext grep matches based on the entire object, not just the beginning.

--- manifests/filecontext.pp
+++ manifests/filecontext.pp
@@ -29,7 +29,7 @@ define selinux::filecontext (
     exec { "semanage_fcontext_${seltype}_${object}":
       command => "semanage fcontext -a -t ${seltype} '${target}'",
       path    => [ '/bin', '/usr/bin', '/sbin', '/usr/sbin' ],
-      unless  => "semanage fcontext -l -C -n | grep ^${object}",
+      unless  => "semanage fcontext -l -C -n | grep ^${object}$",
       require => Package['audit2allow'],
       notify  => Exec["restorecon_${seltype}_${object}"],
     }
